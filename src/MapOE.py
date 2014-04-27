9904fd1e8ce0d174a74251604d1c0d21107e7128
Rohit Bansal <rohit@bansalrohit.com>
2014-04-26 20:25:18 -0400

    Obstacle Expansion Node implemented

---
+++ src/MapOE.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import rospy
+import copy
+from nav_msgs.msg import OccupancyGrid
+
+class MapOE:
+    
+    def beginOE(self, map):
+        
+        # Check to see if Obstacle Expansion is necessary
+        if(self.robotResolution < round(map.info.resolution, 3)):
+            new_map = map
+        else:
+            # Calculate number of blocks to expand obstacles by
+            target_exp = int(round(self.robotResolution/map.info.resolution, 0))
+            
+            # Create a copy of current map message
+            new_map = copy.copy(map)
+            
+            
+            # Figure out the obstacle values for different x,y coordinates
+            MapPosStatus = {}
+            for x in xrange(0, map.info.width):
+                for y in xrange(0, map.info.height):
+                    index = y * map.info.width + x
+                    MapPosStatus[(x,y)] = map.data[index]
+            
+            # create xy pairs for changes in cell locations. 
+            # example, if target_exp = 1, xy = 
+            # [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+            xy = []
+            for i in xrange(-target_exp, target_exp+1):
+                for j in xrange(-target_exp, target_exp+1):
+                    xy.append((i,j));
+            
+            # Store Map occupancy data as a list to allow modification
+            new_data = list(new_map.data)
+            
+            # Expand obstacles
+            for x in xrange(0, map.info.width):
+                for y in xrange(0, map.info.height):
+                    if(MapPosStatus[x,y] == 100):
+                        for element in xy:
+                             index = (y + element[1]) * map.info.width + (x + element[0])
+                             if(index >= 0 and index < len(new_data)):
+                                    new_data[index] =  100
+            # Convert and store data as tuples. 
+            new_map.data = tuple(new_data)
+        
+        self.OEmap.publish(new_map)
+        rospy.sleep(rospy.Duration(4,0))
+        
+    def __init__(self, robotResolution = 0.2):
+        # Initialize Node
+        rospy.init_node('rbansal_vcunha_dbourque_MapOptimization')
+        
+        # Setup publisher and Subscriber
+        self.OEmap = rospy.Publisher('/map_OE', OccupancyGrid, latch=True)
+        self.map = rospy.Subscriber('/map', OccupancyGrid, self.beginOE, queue_size=1)
+        
+        # Store robot resolution
+        self.robotResolution = robotResolution
+        
+# This is the program's main function
+if __name__ == '__main__':
+    map_OE = MapOE()
+    rospy.spin()
