18b02b92e497775426e5c33f307d669a8a267ea1
Rohit Bansal <rbansal@wpi.edu>
2014-04-30 16:39:01 -0700

    Blob search executing. Testing pending

--- src/BlobSearch.py
+++ src/BlobSearch.py
@@ -34,18 +34,20 @@ class BlobSearch:
     
     def getX(self, index):
         x, y = self.getXYindex(index)
-        return x * self.msg.info.resolution + self.msg.origin.position.x
+        return x * self.msg.info.resolution + self.msg.info.origin.position.x
     
     def getY(self, index):
         x, y = self.getXYindex(index)
-        return y * self.msg.info.resolution + self.msg.origin.position.y
+        return y * self.msg.info.resolution + self.msg.info.origin.position.y
 
     def getLabels(self, neighbors):
         label_values = []
         for neighbor in neighbors:
+
             label_val = self.labels[self.getMapIndex(neighbor[0], neighbor[1])]
-            if label_val not in label_values:
+            if label_val != -1:
                 label_values.append(label_val)
+                print "neighbor" + str(label_values)
         return label_values
         
     def isValidPoint(self,x,y):
@@ -60,7 +62,7 @@ class BlobSearch:
     # Gets all connected cells with labels
     def getConnected(self, x, y):
         neighbors = []
-        xy = FrontierSearch.xy
+        xy = BlobSearch.xy
         # check 8 connectivity cells around the current xy
         for i in xrange(0, len(xy)):
             # check if current x, y are in range
@@ -70,6 +72,21 @@ class BlobSearch:
                 if(a_label != -1):
                     neighbors.append((x + xy[i][0], y + xy[i][1]))
         return neighbors
+    
+    def min(self, L):
+        min_val = -1
+        nodes = []
+        node = copy.deepcopy(L)
+        min_val= min(node).pop()
+        for node in L:
+            if(node.__contains__(min_val)):
+                nodes.append(node)
+        min_val = copy.deepcopy(nodes[0])
+        for node in nodes:
+            if(node.__len__() < min_val.__len__()):
+                min_val = copy.deepcopy(node)
+        return min_val
+            
             
     def beginFS(self, msg):
         Background = -1
@@ -88,31 +105,48 @@ class BlobSearch:
                     print "neighbors" + str(neighbors)
                     if neighbors == []:
                         linked.append(set([NextLabel]))                   
-                        self.labels[self.getMapIndex(x,y)] = NextLabel
+                        self.labels[self.getMapIndex(x,y)] = set([NextLabel])
                         NextLabel = NextLabel + 1
                     else:
                        # Find the smallest label
                        L = self.getLabels(neighbors)
                        print "L" + str(L)
-                       self.labels[self.getMapIndex(x,y)] = min(L)
+                       self.labels[self.getMapIndex(x,y)] = self.min(L)
+                       print self.labels[self.getMapIndex(x,y)]
+                       L = self.f7(L)
                        for label in L:
-                           linked[label] = set.union(linked[label], L)
+                           print "L:      " + str(L)
+                           print "linked: " + str(linked)
+                           print
+                           linked[linked.index(label)] = set.union(linked[linked.index(label)] , *L)
         # Second pass
         for x in xrange(0, msg.info.width):
             for y in xrange(0, msg.info.height):
-                if data[self.getMapIndex(x,y)] !=  Background:         
-                     self.labels[self.getMapIndex(x,y)] = set.update(self.labels[self.getMapIndex(x,y)])    
+                if data[self.getMapIndex(x,y)] !=  Background:
+                    label = self.labels[self.getMapIndex(x,y)]        
+                    self.labels[self.getMapIndex(x,y)] = set.update(self.labels[self.getMapIndex(x,y)])    
         print "Done with both passes."
         print "publishing"
-        self.publishGridCells(labels)
+        self.publishGridCells(self.labels)
         
-        
-    def publishGridCells(self, node):
+    def f7(self, seq):
+        seen = []
+        for i in xrange(0, len(seq)):
+            flag = False
+            for j in xrange(i, len(seq)):
+                if(seq[i] == seq[j]):
+                    flag = True
+                    break
+            if not flag:
+                seen.append(seq[i])
+        return seen
+                           
+    def publishGridCells(self, nodes):
         #Initialize gridcell
         gridcells = GridCells()
         gridcells.header.frame_id = 'map'
-        gridcells.cell_width = self.map.info.resolution
-        gridcells.cell_height = self.map.info.resolution
+        gridcells.cell_width = self.msg.info.resolution
+        gridcells.cell_height = self.msg.info.resolution
         
         if nodes == None:
             return
@@ -122,15 +156,15 @@ class BlobSearch:
             point.x = self.getX(i)
             point.y = self.getY(i)
             #Ensure z axis is 0 (2d Map)
-            point.z = node.point.z = 0
+            point.z = 0
             gridcells.cells.append(point)        
-        publisher.publish(gridcells)
+        self.blob_publish.publish(gridcells)
         #rospy.sleep(rospy.Duration(0.05,0))
         print "Done frontier Search"
         
     def __init__(self):
         # Initialize Node
-        rospy.init_node('rbansal_vcunha_dbourque_FrontierSearch')
+        rospy.init_node('rbansal_vcunha_dbourque_BlobSearch')
         
         
         self.msg = None
