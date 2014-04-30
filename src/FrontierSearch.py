9fd20dcd2f243ede88cf1d25273e4ddb06f82c3c
J_Cunha <vcunha@wpi.edu>
2014-04-30 14:21:01 -0400

    This will create the skeleton for the frontier search node necessary DON'T JUDGE

--- src/FrontierSearch.py
+++ src/FrontierSearch.py
@@ -15,14 +15,51 @@ import MapOpt
 
 
 global Searched_Flag
-global 
+global Front_set
+
+
+Front_set = set()
 
 Searched_Flag = True
 
 """
-The purpose of this node is to read MapOE and Map. 
+The purpose of this node is to find out where the frontier is and  
+generate a path towards it. 
+
 """
-def FrontSearc():
+
+class FrontSearch:
+
+   def readMap():
+
+
+ 
+
+
+
+
+
+    def __init__(self, robotResolution = 0.2):
+        # Initialize Node
+        rospy.init_node('rbansal_vcunha_dbourque_MapOE')
+        
+        # Setup publisher and Subscriber
+        self.OEmap = rospy.Publisher('/front_search', OccupancyGrid, latch=True)
+        #self.map = rospy.Subscriber('/map', OccupancyGrid, self.beginOE, queue_size=1)
+        
+        # Store robot resolution (default is 0.2)
+        self.robotResolution = robotResolution
+        
+# This is the program's main function
+if __name__ == '__main__':
+    
+    # Modify this in case of a different robot resolution
+    robotResolution = 0.2
+    
+    # Create MapOE object
+    map_OE = MapOE(robotResolution)
+    rospy.spin()
+
 
     
 
